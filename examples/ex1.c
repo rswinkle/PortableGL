318354b393284a278dcbd189a3c021f26caa13c6
Robert Winkler <rob121618@gmail.com>
2020-10-03 17:57:49 -0600

    Getting examples working on Windows

--- examples/ex1.c
+++ examples/ex1.c
@@ -5,7 +5,7 @@
 
 #include <stdio.h>
 
-
+#define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 
 #define WIDTH 640
@@ -126,6 +126,7 @@ void normal_fs(float* fs_input, Shader_Builtins* builtins, void* uniforms)
 
 void setup_context()
 {
+	SDL_SetMainReady();
 	if (SDL_Init(SDL_INIT_EVERYTHING)) {
 		printf("SDL_init error: %s\n", SDL_GetError());
 		exit(0);
--- examples/ex2.cpp
+++ examples/ex2.cpp
@@ -7,9 +7,10 @@
 
 
 #include <iostream>
-//#include <stdio.h>
+#include <stdio.h>
 
 
+#define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
 
 #define WIDTH 640
@@ -150,6 +151,7 @@ void smooth_fs(float* fs_input, Shader_Builtins* builtins, void* uniforms)
 
 void setup_context()
 {
+	SDL_SetMainReady();
 	if (SDL_Init(SDL_INIT_EVERYTHING)) {
 		cout << "SDL_Init error: " << SDL_GetError() << "\n";
 		exit(0);
