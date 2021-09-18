7dff63e330706bec18885e47eb39f9f81037c0dc
Robert Winkler <rob121618@gmail.com>
2021-09-17 17:06:35 -0700

    Minor tweaks to testing script

--- testing/check_tests.py
+++ testing/check_tests.py
@@ -9,13 +9,27 @@ import sys, os, glob, argparse
 
 
 if __name__ == "__main__":
+
+    results = []
     for image in glob.glob('test_output/*.png'):
         name = image.split('/')[1]
         output = open(image, "rb").read()
         expected = open('expected_output/'+name, "rb").read()
 
-        if output != expected:
-            print(name + " does not match expected output!")
-        else:
-            print(name + " passes")
+        results.append((name, output == expected))
+
+        #if output != expected:
+        #    print(name + " does not match expected output!")
+        #else:
+        #    print(name + " passes")
+
+    # somehow use any/all
+    had_failure = False
+    for test in results:
+        if not test[1]:
+            print(test[0], "FAILED")
+            had_failure = True
+
+    if not had_failure:
+        print("All tests passed")
 
