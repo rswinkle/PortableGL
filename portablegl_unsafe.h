1f04025dc9a8f5c554a3310dc7f726d9ac64aa67
Robert Winkler <rob121618@gmail.com>
2022-04-05 23:47:05 -0700

    Bump version to 0.95
    I'm not ready to actually tag it, but anyone downloading/using
    the latest version should know that it's not 0.94, it's got a decent
    number of fixes/changes/improvements since the 0.94 tag.

--- portablegl_unsafe.h
+++ portablegl_unsafe.h
@@ -1,6 +1,6 @@
 /*
 
-PortableGL 0.94 MIT licensed software renderer that closely mirrors OpenGL 3.x
+PortableGL 0.95 MIT licensed software renderer that closely mirrors OpenGL 3.x
 portablegl.com
 robertwinkler.com
 
@@ -9262,7 +9262,7 @@ GLubyte* glGetString(GLenum name)
 {
 	static GLubyte vendor[] = "Robert Winkler";
 	static GLubyte renderer[] = "PortableGL";
-	static GLubyte version[] = "OpenGL 3.x-ish PortableGL 0.94";
+	static GLubyte version[] = "OpenGL 3.x-ish PortableGL 0.95";
 	static GLubyte shading_language[] = "C/C++";
 
 	switch (name) {
