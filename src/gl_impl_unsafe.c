db4b39252631d512763b8f18d5c9f776d0f8b556
Robert Winkler <rob121618@gmail.com>
2022-07-16 14:24:11 -0700

    Bump version to 0.96

--- src/gl_impl_unsafe.c
+++ src/gl_impl_unsafe.c
@@ -349,7 +349,7 @@ GLubyte* glGetString(GLenum name)
 {
 	static GLubyte vendor[] = "Robert Winkler";
 	static GLubyte renderer[] = "PortableGL";
-	static GLubyte version[] = "OpenGL 3.x-ish PortableGL 0.95";
+	static GLubyte version[] = "OpenGL 3.x-ish PortableGL 0.96";
 	static GLubyte shading_language[] = "C/C++";
 
 	switch (name) {
